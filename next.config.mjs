3c5fa71601f2db71be43e65c428073a59024efbf
Xavier Damman <xdamman@gmail.com>
2024-05-06 15:30:20 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        { source: "/", destination: "https://fun-closet-618154.framer.app" },
+        { source: "/:path*", destination: "https://fun-closet-618154.framer.app/:path*"}
+      ],
+    };
+  }
+}
 export default nextConfig;
